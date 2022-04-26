15f1e0c13281ba48a9bb5caa45601be8f01ac1b2
Sasa Kocic <sasa.kocic@gmail.com>
2022-04-26 17:19:57 +0200

    - added tests for increasing code coverage to 80.56%

--- test/controllers/comments_controller_test.rb
+++ test/controllers/comments_controller_test.rb
@@ -1,7 +1,13 @@
 require "test_helper"
 
 class CommentsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @post = posts(:one)
+    @comment = comments(:one)
+  end
+
+  test "should post comment" do
+    post post_comments_url(post_id: @post.id, comment: { content: "Test comment" })
+    assert_redirected_to post_url(id: @post.id)
+  end
 end
